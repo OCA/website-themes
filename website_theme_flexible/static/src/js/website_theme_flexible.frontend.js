1f5e5cdeb76c1e7c3e289252da5150b2996c849d
tarteo <d.sluijk@onestein.nl>
2018-03-13 08:57:14 +0100

    [ADD] 11.0 website_theme_flexible
    [ADD] javascript libs (removed from .gitignore)
    
    [ADD] navbar toggler
    [ADD] menu font
    [ADD] option 'None' for logo / name allignment
    [ADD] Custom css tab
    
    [FIX] sticky menu
    
    [IMP] Responsiveness of theme_customize dialog

---
+++ website_theme_flexible/static/src/js/website_theme_flexible.frontend.js
@@ -0,0 +1,19 @@
+/* Copyright 2018 Onestein
+ * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
+
+odoo.define('website_theme_flexible.frontend', function(require) {
+"use strict";
+
+    var base = require('web_editor.base');
+
+    base.ready().then(function () {
+        var header = $('header');
+        var navbar = $('header > .navbar');
+
+        if (navbar.attr('data-do-stick') === '1') {
+            var navbar_clone = navbar.clone();
+            navbar_clone.addClass('navbar-fixed-top');
+            header.append(navbar_clone);
+        }
+    });
+});
